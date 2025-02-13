3774d15735f7b90bf0769c41afe0d62744f4053c
Simret Belete <104854158+Simret101@users.noreply.github.com>
2025-02-13 10:32:02 +0300

    Delete blogservice directory

--- blogservice/domain/bookmark.model.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Bookmark struct {
-	ID     primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
-	BlogID primitive.ObjectID `json:"blog_id" bson:"blog_id"`
-}
