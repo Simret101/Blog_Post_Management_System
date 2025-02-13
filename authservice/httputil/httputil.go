936fecfcdf3d88de22fcdf82a73ebe8608929497
Simret Belete <104854158+Simret101@users.noreply.github.com>
2025-02-13 10:31:39 +0300

    Delete authservice directory

--- authservice/httputil/httputil.go
+++
@@ -1,7 +0,0 @@
-package httputil
-
-// HTTPError represents an error that occurred while handling a request.
-type HTTPError struct {
-	Code    int    `json:"code" example:"400"`
-	Message string `json:"message" example:"Invalid request"`
-}
